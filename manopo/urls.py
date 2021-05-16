5249c890512fab9fc7772bb7baa9c9f12296d375
mnabeel <71066857+n48331@users.noreply.github.com>
2021-05-16 20:47:15 +0530

    make it better

--- manopo/urls.py
+++ manopo/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/', include('search.urls')),
     path('',include('accounts.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+ 
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
