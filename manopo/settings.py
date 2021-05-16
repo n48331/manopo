3092d033ed5b69cf184779898e314397da3e0df8
mnabeel <71066857+n48331@users.noreply.github.com>
2021-05-16 19:54:06 +0530

    database change

--- manopo/settings.py
+++ manopo/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'manopo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'manopo',
-        'USER':'postgres',
-        'PASSWORD' : '1234',
-        'HOST' : 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3')
     }
 }
 
